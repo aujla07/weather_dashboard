e21196048c64f9165213afb1e385615ece749598
Aman Aujla <aujla7736@gmail.com>
2020-10-21 17:55:28 -0500

    updated code in index.html

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+// document.querySelector(".btn").addEventListener("click",function(event){
+//     console.log("clicked")
+// })
+const API_KEY ="8b267c19ed20a20ec0f113d0fe760b1b"
+//current weather
+
+//uv index
+
+$(".btn").on("click", function(event){
+    var userinput= $("input").val()
+    console.log(userinput)
+})
